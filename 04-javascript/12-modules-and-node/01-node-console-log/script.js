cd65930163a33e804754c53acd1d71cbaf937cc1
Xaver Ibarra <>
2022-12-01 18:28:19 -0600

    finished 12-modules-and-node 01-node-console activity

--- 04-javascript/12-modules-and-node/01-node-console-log/script.js
+++ 04-javascript/12-modules-and-node/01-node-console-log/script.js
@@ -1,1 +1,11 @@
 // Place a string into a console.log() and then run the script in the terminal using 'node script.js'
+const readline = require('readline').createInterface ({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question("Enter Number:", num => {
+    const sum = num*num;
+    console.log(`Square of ${num} is ${sum}!`);
+    readline.close();
+});
